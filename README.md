02746df73a6956d3f93865d22fb48b7a26268174
Joze Fartek <joze.fartek@gmail.com>
2020-01-30 10:35:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# shopify-wordpress-posts-parser
---
+++ main.py
@@ -0,0 +1,67 @@
+import json
+import csv
+import arrow
+import requests
+
+authors = {
+    "Emily Bertha": "emilybertha",
+    "Guest Writer": "eightguestwriter",
+    "Alex Pinarello": "eightalexpinarello",
+    "Carlie Dobkin": "eightcarliedobkin",
+    "Lucid Fusion Collaborator": "eightguestwriter",
+    "Alexandra Zatarain": "eightalexandrazatarain",
+    "Eight Collaborator": "eightguestwriter",
+    "Matteo Franceschetti": "eightmatteofranceschetti"
+}
+
+with open('out.csv', mode='w') as csv_file:
+    fieldnames = ['post_title', 'ID', 'post_content', 'post_excerpt', 'post_date', 'post_name', 'post_author',
+                  'post_publisher',
+                  'post_status', 'featured_image', 'post_format', 'comment_status', 'ping_status', 'post_category',
+                  'post_tag'
+                  ]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    with open('articles_20200114.json') as json_file:
+        data = json.load(json_file)
+        count = 0
+        for a in data['articles']:
+            count += 1
+            if count > 20:
+                continue
+
+            r = requests.get('http://eightsleep.com/blogs/news/' + a['handle'])
+            print(r.status_code)
+            if r.status_code == 404:
+                print('404: Post: ' + str(a['id']), a['title'], a['handle'], date_str)
+                continue
+
+            image_url = ''
+            if 'image' in a:
+                image_url = a['image']['src']
+
+            date = arrow.get(a['created_at'])
+            d = date.datetime
+
+            dMinute = ("0" + str(d.minute) if d.minute < 10 else str(d.minute))
+            date_str = str(d.month) + "/" + str(d.day) + "/" + str(d.year) + " " + str(d.hour) + ":" + dMinute
+
+            print('Post: ' + str(a['id']), a['title'], a['handle'], date_str)
+            writer.writerow({
+                'post_title': a['title'],
+                'ID': str(a['id']),
+                'post_content': a['body_html'],
+                'post_excerpt': '',
+                'post_date': date_str,
+                'post_name': a['handle'],
+                'post_author': authors[a['author']],
+                'post_publisher': a['author'],
+                'post_status': 'publish',
+                'featured_image': image_url,
+                'post_format': '',
+                'comment_status': 'open',
+                'ping_status': 'open',
+                'post_category': 'Uncategorized',
+                'post_tag': a['tags']
+            })
